12f032ba541973019f7f98590c31b16916278049
DIR\ramon.silva <ramon.silva@concretesolutions.com.br>
2018-08-15 19:31:54 -0300

    Continuação a introdução Toast

--- DemoApp/app/src/main/java/com/ramonsilva/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/com/ramonsilva/demoapp/MainActivity.java
@@ -2,8 +2,21 @@ package com.ramonsilva.demoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void showInfo(View view) {
+        EditText myEditText = findViewById(R.id.idTxtViewMyTxt);
+        String myText = myEditText.getText().toString();
+        if(myText.length() > 0)
+            Toast.makeText(MainActivity.this, myText, Toast.LENGTH_LONG).show();
+
+        Log.i("ButtonClick","My button was clicked!");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
