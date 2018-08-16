555668ed87832ca8468b67428267710a5523218a
DIR\ramon.silva <ramon.silva@concretesolutions.com.br>
2018-08-16 17:40:17 -0300

    Currency App

--- DemoApp/app/src/main/java/com/ramonsilva/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/com/ramonsilva/demoapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.ramonsilva.demoapp;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, myText, Toast.LENGTH_LONG).show();
 
         Log.i("ButtonClick","My button was clicked!");
+    }
+
+    public void changeImage(View view) {
+        ImageView myImage = findViewById(R.id.idImgView);
+        if(myImage.getDrawable().getConstantState() == getResources().getDrawable(R.drawable.image2).getConstantState()) {
+            myImage.setImageResource(R.drawable.image1);
+        } else {
+            myImage.setImageResource(R.drawable.image2);
+        }
     }
 
     @Override
